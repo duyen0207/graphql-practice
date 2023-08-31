eb93c23505dde574495ceef61cee94c167ce42c1
Duyên Lê <duyenltb@bsscommerce.com>
2023-08-31 14:10:39 +0700

    feat: query variable

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// db
+import db from "./_database.js";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+};
+
 // server setup
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  // type defs - definitions of types of data we want to expose on our graph
+  typeDefs: typeDefs,
+  // resolvers - handle respond
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
